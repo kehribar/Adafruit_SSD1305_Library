aaed9204356c1e661d987df05a873b880a10a30c
kehribar <ihsan@kehribar.me>
2015-12-10 00:35:41 +0200

    128x32 SSD1305 specific fixes.

--- Adafruit_SSD1305.cpp
+++ Adafruit_SSD1305.cpp
@@ -183,7 +183,7 @@ void Adafruit_SSD1305::begin(uint8_t i2caddr) {
     digitalWrite(rst, HIGH);
   }
 
-#if defined SSD1306_128_32
+  // #if defined SSD1306_128_32
   // Init sequence for 128x32 OLED module
   command(SSD1305_DISPLAYOFF);                    // 0xAE
   command(SSD1305_SETLOWCOLUMN | 0x0);  // low col = 0
@@ -217,44 +217,43 @@ void Adafruit_SSD1305::begin(uint8_t i2caddr) {
   command(0x3F);
   command(0x3F);
   command(0x3F);
+  // #endif
 
-  #endif
+  // #if defined SSD1305_128_64
+  // // Init sequence for 128x64 OLED module
+  // command(SSD1305_DISPLAYOFF);                    // 0xAE
+  // command(SSD1305_SETLOWCOLUMN | 0x4);  // low col = 0
+  // command(SSD1305_SETHIGHCOLUMN | 0x4);  // hi col = 0
+  // command(SSD1305_SETSTARTLINE | 0x0); // line #0
+  // command(0x2E); //??
+  // command(SSD1305_SETCONTRAST);                   // 0x81
+  // command(0x32);
+  // command(SSD1305_SETBRIGHTNESS);                 // 0x82
+  // command(0x80);
+  // command(SSD1305_SEGREMAP | 0x01);
+  // command(SSD1305_NORMALDISPLAY);                 // 0xA6
+  // command(SSD1305_SETMULTIPLEX);                  // 0xA8
+  // command(0x3F); // 1/64
+  // command(SSD1305_MASTERCONFIG);
+  // command(0x8e); /* external vcc supply */
+  // command(SSD1305_COMSCANDEC);
+  // command(SSD1305_SETDISPLAYOFFSET);              // 0xD3
+  // command(0x40); 
+  // command(SSD1305_SETDISPLAYCLOCKDIV);            // 0xD5
+  // command(0xf0); 
+  // command(SSD1305_SETAREACOLOR);  
+  // command(0x05);
+  // command(SSD1305_SETPRECHARGE);                  // 0xd9
+  // command(0xF1);
+  // command(SSD1305_SETCOMPINS);                    // 0xDA
+  // command(0x12);
 
-  #if defined SSD1305_128_64
-  // Init sequence for 128x64 OLED module
-  command(SSD1305_DISPLAYOFF);                    // 0xAE
-  command(SSD1305_SETLOWCOLUMN | 0x4);  // low col = 0
-  command(SSD1305_SETHIGHCOLUMN | 0x4);  // hi col = 0
-  command(SSD1305_SETSTARTLINE | 0x0); // line #0
-  command(0x2E); //??
-  command(SSD1305_SETCONTRAST);                   // 0x81
-  command(0x32);
-  command(SSD1305_SETBRIGHTNESS);                 // 0x82
-  command(0x80);
-  command(SSD1305_SEGREMAP | 0x01);
-  command(SSD1305_NORMALDISPLAY);                 // 0xA6
-  command(SSD1305_SETMULTIPLEX);                  // 0xA8
-  command(0x3F); // 1/64
-  command(SSD1305_MASTERCONFIG);
-  command(0x8e); /* external vcc supply */
-  command(SSD1305_COMSCANDEC);
-  command(SSD1305_SETDISPLAYOFFSET);              // 0xD3
-  command(0x40); 
-  command(SSD1305_SETDISPLAYCLOCKDIV);            // 0xD5
-  command(0xf0); 
-  command(SSD1305_SETAREACOLOR);  
-  command(0x05);
-  command(SSD1305_SETPRECHARGE);                  // 0xd9
-  command(0xF1);
-  command(SSD1305_SETCOMPINS);                    // 0xDA
-  command(0x12);
-
-  command(SSD1305_SETLUT);
-  command(0x3F);
-  command(0x3F);
-  command(0x3F);
-  command(0x3F);
-  #endif
+  // command(SSD1305_SETLUT);
+  // command(0x3F);
+  // command(0x3F);
+  // command(0x3F);
+  // command(0x3F);
+  // #endif
 
   command(SSD1305_DISPLAYON);//--turn on oled panel
 }
@@ -343,7 +342,7 @@ void Adafruit_SSD1305::display(void) {
   for(; page<8; page++)
   {
     command(SSD1305_SETPAGESTART + page);
-    command(0x00);
+    command(0x04); // command(0x00);
     command(0x10);
 
     if (cs == -1) {
